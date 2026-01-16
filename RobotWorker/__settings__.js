9d3334cbb53b068951fbf73225923b0939a5a8b9
liukefu <liukefu2050@gmail.com>
2026-01-16 15:42:53 +0800

    playCanvas

---
+++ RobotWorker/__settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2404891.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 271189178, 271201005, 271202483, 271204014, 271204189, 271287987 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/271209107/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/271209108/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/271209106/1/ammo.js', 'preload' : true},
+];
